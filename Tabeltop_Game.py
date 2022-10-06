cede719261b5a90f2c0dc2909eafa129586e41b5
GreeneGunnar14 <bryce.greene@students.fhu.edu>
2022-10-06 15:00:12 -0500

    More work on program

--- Tabeltop_Game.py
+++ Tabeltop_Game.py
@@ -19,7 +19,7 @@ class __TableTopGame:
     self.initial_event_list = []
     self.initial_event = None
     self.additional_event_list = []
-    self.active_events = []
+    self.active_events = {}
     self.event_descriptions_file = descriptions
     #TODO add the main menu message
     self.main_menu_message = "Welcome to your tabletop game.\nPlease choose an option below."
@@ -27,24 +27,31 @@ class __TableTopGame:
   #FIXME
   def Start(self):
     if not self.is_running:
-      self.Loop()
+      self.Run()
     else:
-      self.active_events = []
+      self.active_events.clear()
       for event in self.additional_events:
         event.status = Status.inactive
       self.initial_event.status = Status.inactive
       self.initial_event = None
-      self.Loop()
+      self.Run()
   
   def End(self):
-    exit()
+    exit(0)
   
-  #TODO
-  def DisplayMenu(self, message=None, event=None):
+  #TODO finish displaying the full menu.
+  def DisplayMenu(self, message=None, event=None, start_menu=False):
+    system('clear')
     if not message:
       message = self.main_menu_message
-    
+    if start_menu:
+      system('clear')
+      print(message + '\n\n')
+      print('1. Start game with random initial event.\n2. Choose initial event and start game.\n3. Exit\n\n')
+    else:
+      raise NotImplementedError()
 
+  #Return a random integer between two given values
   def RollDie(minimum, maximum):
     result = randint(minimum, maximum)
     return result
@@ -53,13 +60,22 @@ class __TableTopGame:
     filename = input("Enter location for event descriptions.")
     self.event_descriptions_file = filename
   
-  def Loop(self):
-    pass
-    #TODO get input, begin loop
+  def Run(self):
+    self.DisplayMenu(start_menu=True)
+    user_input = input('Selection: ')
+    while not user_input in ('1', '2', '3'):
+      self.DisplayMenu(start_menu=True)
+      user_input = input('Input not recognized. Try again: ')
+    #FIXME This is here to test that Run and Display Menu are working
+    return user_input
 
-  #Set an event to active
-  def SetEvent(self, event):
+  #Update the status of an event, update self.active_events accordingly
+  def UpdateEvent(self, event):
     event.UpdateStatus()
+    if event.status == Status.active:
+      self.active_events.add(event)
+    elif event.status == Status.completed:
+      self.active_events.remove(event)
 
   #Go through the events file and extract all the events
   def GetDescriptions(self):
@@ -73,15 +89,18 @@ class __TableTopGame:
         description = line[13:]
       elif line[0:12] == "Best Practice":
         best_practice = line[15:]
-        event = Event(name, description, best_practice)
+        event = __Event(name, description, best_practice)
         if use == "initial":
-          self.initial_event_list += event
+          self.initial_event_list.append(event)
         elif use == "additional":
-          self.additional_events += event
+          self.additional_event_list.append(event)
         else:
           raise Exception("Event use (initial/additional) not set properly")
       else:
         use = line.lower() if line.lower() == "initial" or line.lower() == "additional" else None
+
+  def SelectInitialEvent(self):
+    raise NotImplementedError()
 
   #TODO Finish filling out TableTopGame class
 
